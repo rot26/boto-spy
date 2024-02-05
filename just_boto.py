f3d54627438fb2a6a6cb5f03963ca5ab74ef4be0
Chris Coleman <chris@rot-26.com>
2024-02-05 14:46:17 -0700

    playing with boto calls

--- just_boto.py
+++ just_boto.py
@@ -3,27 +3,35 @@ import boto3
 session = boto3.Session(region_name='us-east-1')
 print(session)
 
+def handle_after_call(*args, **kwargs):
+    print(kwargs.get("event_name",None))
+    print(kwargs.get("parsed", {}).get("ResponseMetadata", {}).get("RequestId", None))
+    print(kwargs.get("parsed", {}).get("ResponseMetadata", {}).get("HTTPStatusCode", None))
+
+def handle_provide_client_params(*args, **kwargs):
+    print(kwargs.get("event_name",None))
+    print(kwargs.get("params",None))
+
 def handle_events(*args, **kwargs):
     # print(args)
     # print(kwargs)
     print(kwargs.get("event_name"))
     print("\t"+str(list(kwargs.keys())))
 
-
-def do_calls():
+def list_functions():
     aws_lambda = session.client('lambda')
     functions = aws_lambda.list_functions()
-    # aws_lambda.list_layers(CompatibleRuntime="python3.10")
-    # print(functions)
-    # Now make Boto3 calls using the default session.
-    # client = session.client('ec2')
-    # client.describe_images(DryRun=True)
-    # images = client.describe_images(DryRun=False)
+
+def get_caller():
+    client = session.client('sts')
+    client.get_caller_identity()
 
 def register():
-    event='*.*.*'
-    session.events.register(event, handle_events, 'best-test')
+    # session.events.register('*.*.*',handle_events, 'best-test')
+    session.events.register('after-call.*.*', handle_after_call, 'after-call-spy')
+    session.events.register('provide-client-params.*.*', handle_provide_client_params, 'provide-client-params-spy')
 
 register()
 
-do_calls()
+# list_functions()
+get_caller()
